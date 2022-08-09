f73fc94b7e71ce35fcab9d6d20208663fba25da1
Dev Nexus <osunkiyesitayo@gmail.com>
2022-08-09 14:44:55 +0100

    Added webhook endpoint

--- routes/paystackRoutes.js
+++ routes/paystackRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getCountries,
   getBanks,
+  webhook
 } = require("../controllers/paystackControllers");
 
 const router = express.Router();
 
 router.route("/paystack/countries").get(getCountries);
 router.route("/paystack/banks/:country").get(getBanks);
+router.route("/paystack/webhook").get(webhook);
 
 module.exports = router;
